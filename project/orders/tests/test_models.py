30f8d550e253886fa4fa82aca5d846f7a2ddc46b
Joseph Victor Zammit <jvzammit@gmail.com>
2023-10-03 14:59:32 +0200

    have whitespace separate given/when/then

--- project/orders/tests/test_models.py
+++ project/orders/tests/test_models.py
@@ -42,7 +42,6 @@ class OrderQuerysetTests(TestCase):
         date1 = arrow.get("2023-09-09 23:59").datetime  # yesterday
         date2 = arrow.get("2023-09-10 00:00").datetime  # today 00:00
         date3 = arrow.get("2023-09-10 00:01").datetime  # today 00:01
-
         _ = OrderFactory(created_at=date1)
         order2 = OrderFactory(created_at=date2)
         order3 = OrderFactory(created_at=date3)
